1a4ce5351512f11dd486532d7aa417f1a7ea464a
Robert Seliga <robertMseliga@gmail.com>
2018-07-24 09:57:44 -0400

    add a gitignore and more code

--- botFile.py
+++ botFile.py
@@ -2,6 +2,10 @@ import sc2
 from sc2 import run_game, Maps, Race, Difficulty
 from sc2.player import Bot, Computer
 
+
 class SentdeBot(sc2.BotAI):
 	async def on_step(self, iteration):
-		
+		#distributing the workers
+		await self.distribute_workers()
+
+run_game()
